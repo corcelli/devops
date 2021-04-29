58ad9b7dc48dcc03d0ca04dddd3dad6cb9ea8ae7
Tiago Corcelli de Oliveira <34511305+corcelli@users.noreply.github.com>
2021-04-29 11:09:31 -0300

    Update install_docker_debian_v1.sh

--- install_docker_debian_v1.sh
+++ install_docker_debian_v1.sh
@@ -7,5 +7,6 @@ apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker.service
 systemctl start docker.service
-
+curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker run -d -p 9000:9000 -p 8000:8000 --restart=always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce --admin-password='$2y$05$bzntTUAjYqWrB9I6XXWDM.tRUMH8LM5Sk3xy0Ye/94Y4gh2toBCYC'
